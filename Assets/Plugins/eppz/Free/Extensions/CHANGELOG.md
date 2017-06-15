bd066033f26252fbf04d451e7bad35158a4e86d7
eppz <hello@eppz.eu>
2017-06-15 18:14:21 +0200

    0.0.0

---
+++ Assets/Plugins/eppz/Free/Extensions/CHANGELOG.md
@@ -0,0 +1,5 @@
+* 0.0.0
+
+	+ Initial commit
+		+ Added `String_Extensions`
+		+ Namespaced (into the folder it resides)
---
+++ Assets/Plugins/eppz/Free/Extensions/String.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace EPPZ.Free.Extensions
+{
+
+
+	public static class String_Extensions
+	{
+
+
+		// Mainly from http://stackoverflow.com/questions/3174152/isnulloremptyorwhitespace-problem-in-c-sharp
+
+
+		public static bool IsNullOrWhiteSpace(this string value)
+		{
+			if (value != null)
+			{
+				for (int i = 0; i < value.Length; i++)
+				{
+					if (!char.IsWhiteSpace(value[i]))
+					{
+						return false;
+					}
+				}
+			}
+			return true;
+		}
+
+		public static bool IsNullOrEmptyOrWhiteSpace(this string value)
+		{
+			return string.IsNullOrEmpty(value) ||
+				ReferenceEquals(value, null) ||
+					string.IsNullOrEmpty(value.Trim(' '));
+		}
+
+		public static string RemoveLastWord(this string value)
+		{
+			if (value == null) return ""; // Only if any
+
+			string result = value;
+			int index = value.LastIndexOf(" ");
+			if (index > -1) result = value.Remove(index);
+			return result;
+		}
+	}
+}
